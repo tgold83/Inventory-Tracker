2bb8c2d4fd100a3c2a764b7808bd13208a57a614
tgold83 <ted.jonathan.gold@gmail.com>
2022-11-18 09:31:45 -0800

    add code to coffe.js

---
+++ src/components/Coffee.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Coffee(props){
+  return(
+    <React.Fragment>
+      <div onClick={() => props.whenCoffeeClicked(props.id)}>
+        <h2>{props.name}</h2>
+        <h5>Click for more info about this type of coffee.</h5>
+        <hr/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Coffee.propTypes = {
+  name: PropTypes.string.isRequired,
+  origin: PropTypes.string.isRequired,
+  roast: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  quantity: PropTypes.number.isRequired, 
+  id: PropTypes.string,
+  whenCoffeeClicked: PropTypes.func
+};
+
+export default Coffee;
