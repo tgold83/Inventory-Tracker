06a211e40e38e628c36fb98a5c8b30d5068a0980
tgold83 <ted.jonathan.gold@gmail.com>
2022-11-18 11:09:35 -0800

    add code to coffeelist

---
+++ src/components/CoffeeList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Coffee from "./Coffee";
+import PropTypes from "prop-types";
+
+function CoffeeList(props){
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.coffeeList.map((coffee) =>
+        <Coffee 
+          whenCoffeeClicked = { props.onCoffeeSelection }
+          name={coffee.name}
+          origin={coffee.origin}
+          roast={coffee.roast}
+          price={coffee.price}
+          quantity={coffee.quantity}
+          id={coffee.id}
+          key={coffee.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+CoffeeList.propTypes = {
+  coffeeList: PropTypes.array,
+  onCoffeeSelection: PropTypes.func
+};
+
+export default CoffeeList;
