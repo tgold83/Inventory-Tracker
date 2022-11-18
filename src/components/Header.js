8c149d36671a4b0a6cb3b3c0c9aaa01e71c5f90b
tgold83 <ted.jonathan.gold@gmail.com>
2022-11-18 10:06:00 -0800

    add code to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(){
   return(
     <React.Fragment>
       <h1>Welcome to CamelCaseCoffee</h1>
-      <h3>Our MAIN FUNCTION is to supply you with the the best JAVA</h3>
+      <h3>Our MAIN FUNCTION is to supply you with the the best JAVA!</h3>
       <img src={CamelCaseCoffee} alt="coffee logo" width='15%' />
     </React.Fragment>
   );
