a4112303b5359d4402ff69e6ca039a0693f091d0
tgold83 <ted.jonathan.gold@gmail.com>
2022-11-18 09:02:01 -0800

    add code to header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CamelCaseCoffee from "./../img/CamelCaseCoffee.png";
+
+function Header(){
+  return(
+    <React.Fragment>
+      <h1>Welcome to CamelCaseCoffee</h1>
+      <h3>Our MAIN FUNCTION is to supply you with the the best JAVA</h3>
+      <img src={CamelCaseCoffee} alt="coffee logo" width='15%' />
+    </React.Fragment>
+  );
+}
+
+export default Header;
